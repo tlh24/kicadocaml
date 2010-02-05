9d687553820c4e8cc77395356442aef44b434da1
sideskate <sideskate@7fc34402-f330-11dd-802b-177316e4ac06>
2010-02-05 22:27:35 +0000

    simulated annealing ... eh, it's okay. needs more work, clearly.

--- trunk/anneal.ml
+++ trunk/anneal.ml
@@ -31,9 +31,12 @@ class a_pad = object (self)
 		let v = List.fold_left (fun s p -> 
 			Pts2.add s (Pts2.sub (p#getPos ()) mypos)
 		) (0.0,0.0) m_conn in
+		(* printf "suggest: list length %d\n%!" (List.length m_conn);
+		printf " sum: %f,%f\n%!" (fst v) (snd v); *)
 		Pts2.scl v (foi (List.length m_conn))
 end and a_mod = object
 	val mutable m_mod = new pcb_module
+	val mutable m_offset = 0.0,0.0 (* subtract this when setting m_mod's position *)
 	val mutable mx = 0.0
 	val mutable my = 0.0
 	val mutable mr = 0.0
@@ -45,9 +48,12 @@ end and a_mod = object
 	method getMod () = m_mod
 	method setPos (kx,ky) = mx <- kx; my <- ky; 
 	method setRot r = mr <- r;
+	method getRot () = mr;
 	method setPads (pds: a_pad list) = m_pads <- pds
 	method getPads () = m_pads
 	method setWH (w,h) = mw <- w; mh <- h; 
+	method getWH () = mw,mh
+	method setOffset o = m_offset <- o; 
 	method move (kx,ky) = mx <- mx +. kx; my <- my +. ky; 
 	method transform k = (* transform pad coordinates to global *)
 		Pts2.add (rotate2 ~angle:(deg2rad mr) k ) (mx,my)
@@ -55,16 +61,13 @@ end and a_mod = object
 		let w,h = if cos(deg2rad mr) > 0.707 then mw,mh else mh,mw in
 		mx -. w, my -. h, mx +. w, my +. h
 	method update () = (* send the new loc to the parent *)
-		m_mod#setRot (iof mr); 
-		m_mod#move (mx,my); 
+		m_mod#setRot (iof mr * 10); 
+		m_mod#setPos (Pts2.sub (mx,my) (rotate2 ~angle:(deg2rad mr) m_offset )); 
 		m_mod#setMoving false; 
 end
 
-let doAnneal mods render = 
-	let temp = 0.002 in (* in mils *)
+let doAnneal mods render temp passes = 
 	let k = ref 0 in
-	List.iter (fun m -> m#setRot 0) mods; 
-	List.iter (fun m -> m#update ()) mods; 
 	List.iter (fun m -> m#setMoving true) mods; 
 
 	(* first, copy the data structures (bad, I know, but my mind is limited.. *)
@@ -72,15 +75,21 @@ let doAnneal mods render =
 	let amods = List.map (fun m -> 
 		let am = new a_mod in
 		am#setMod m; 
-		am#setPos (m#getPos ()); 
-		am#setRot (foi (m#getRot ())); 
+		m#update () ;
+		am#setPos (m#getCenter false); 
+		am#setRot (foi (m#getRot ())/.10.0); 
 		m#setRot 0; 
+		m#setPos (0.0, 0.0); 
 		m#update () ; (* to get a proper bbx, will change back later *)
-		am#setWH (Pts2.scl (bbxWH (m#getBBX false)) 0.5); 
+		let w, h = Pts2.scl (bbxWH (m#getBBX false)) 0.75 in
+		let offset = bbxCenter (m#getBBX false) in
+		am#setWH (w,h); 
+		am#setOffset offset;
+		(*printf "module %s w %f h %f rot %f\n%!" (m#getRef()) w h (am#getRot()); *)
 		am#setPads (List.map (fun p ->
 			let ap = new a_pad in
 			ap#setParent am; 
-			ap#setPos (p#getPos ());
+			ap#setPos (Pts2.sub (p#getPos ()) offset);
 			let net = p#getNet () in
 			ap#setNet net;
 			if net > !maxnet then maxnet := net ; 
@@ -88,7 +97,7 @@ let doAnneal mods render =
 			ap
 		) (m#getPads ()) ); 
 		am
-	) mods in
+	) (List.filter (fun m -> m#getVisible()) mods) in
 	(* now need to make an array of lists of pads connected to each net *)
 	let nets = Array.mapi (fun i j -> 
 		List.fold_left (fun j1 m -> 
@@ -96,47 +105,99 @@ let doAnneal mods render =
 				if p#getNet () = i then p::j2 else j2
 			) j1 (m#getPads ())
 		) j amods; 
-	) ( Array.make !maxnet [] ) in
+	) ( Array.make (!maxnet+1) [] ) in
 	(* ...and go back and give the pads these lists *)
 	List.iter (fun m -> 
 		List.iter (fun p -> 
 			let n = p#getNet() in
 			(* don't self-reference! *)
-			p#setConn (List.filter (fun pp -> pp <> p) (nets.(n))); 
+			if(n >= 0 && n <= !maxnet) then (
+				let lst = nets.(n) in
+				if List.length lst < 12 then ( (* ignore the larger nets *)
+					p#setConn (List.filter (fun pp -> pp <> p) lst); 
+				)
+			)
 		) (m#getPads()); 
 	) amods; 
-	while !k < 10 do (
+	let renderAll () = 
+		List.iter (fun m -> 
+			m#update (); 
+		) amods; 
+		render (); 
+	in
+	while !k < passes do (
+		(* inject som noise *)
 		List.iter (fun m -> 
 			m#move (((Random.float temp) -. temp/.2.),((Random.float temp) -. temp/.2.)); 
-			m#update (); 
 		) amods; 
-		render (); 
-		(* now see where they are hitting, and move them accordingly *)
-		List.iter (fun m1 -> 
-			let bbx1 = m1#getBBX () in
-			List.iter (fun m2 -> 
-				let bbx2 = m2#getBBX () in
-				if bbxIntersect bbx1 bbx2 then (
-					(* move the modlues accordingly *)
-					let w1,h1 = bbxWH bbx1 in
-					let w2,h2 = bbxWH bbx1 in
-					let x1,y1 = bbxCenter bbx1 in
-					let x2,y2 = bbxCenter bbx2 in
-					let dx,dy = x2 -. x1,y2 -. y1 in
-					let calcmov d w = if fabs d < w then ((w -. (fabs d)) *. (fsign d)) else 0.0 in
-					let mx,my = (calcmov dx (w1 +. w2)),(calcmov dy (h1 +. h2)) in
-					(* note, you only need one of these - rectanguar objects*)
-					let move = if (fabs mx) < (fabs my) then mx, 0.0 else 0.0, my in
-					m1#move (Pts2.scl move (-0.25)); (* tweak this ? *)
-					m2#move (Pts2.scl move 0.25); 
-					(* clearly, this will have to run for a while ... *)
-				)
-			) amods; 
-		) amods; 
+		(* renderAll (); *)
+
+		(* move them towards other conneted elements *)
 		List.iter (fun m -> 
-			m#update (); 
-		) amods; 
-		render (); 
+			let sug = List.fold_left (fun s p -> 
+				Pts2.add s (p#suggest())
+			) (0.0,0.0) (m#getPads()) in
+			let len = List.length (m#getPads()) in
+			let mv = Pts2.scl sug (1.0/.((foi len) *. (8.0 +. (Random.float 5.0)))) in
+			m#move mv; 
+		) amods ; 
+		
+		(* rotate if that will help *)
+		List.iter (fun m -> 
+			let getCost () = List.fold_left (fun s p -> 
+				s +. p#cost()
+			) 0.0 (m#getPads()) in
+			let costs = List.map (fun r -> m#setRot r; (r,getCost())) [0.0;90.0;180.0;270.0] in
+			(*List.iter(fun a -> 
+				printf "rot %f cost %f\n%!" (fst a) (snd a); 
+			) costs; *)
+			let sort = List.sort (fun a b -> compare (snd a) (snd b)) costs in
+			let best = List.hd sort in
+			(*printf "best %f\n%!" (fst best); *)
+			m#setRot (fst best); 
+		) amods ; 
+		(* renderAll (); *)
+		
+		(* now see where they are hitting, and move them accordingly *)
+		let hitAll () = 
+			let mvcnt = ref 1 in
+			let passes = ref 0 in
+			while !mvcnt > 0 && !passes < 20 do (
+				mvcnt := 0;
+				List.iter (fun m1 -> 
+					List.iter (fun m2 -> 
+						if m1 <> m2 then (
+							let bbx1 = m1#getBBX () in
+							let bbx2 = m2#getBBX () in
+							(* let printbbx (x,y,xx,yy) = printf "(%f,%f %f,%f)\n%!" x y xx yy in
+							printbbx bbx1; 
+							printbbx bbx2; *)
+							if bbxIntersect bbx1 bbx2 then (
+								(* move the modlues accordingly *)
+								let w1,h1 = bbxWH bbx1 in
+								let w2,h2 = bbxWH bbx2 in
+								let x1,y1 = bbxCenter bbx1 in
+								let x2,y2 = bbxCenter bbx2 in
+								let dx,dy = x2 -. x1,y2 -. y1 in
+								let calcmov d w = if fabs d < w then ((0.5*.w -. (fabs d)) *. (fsign d)) else 0.0 in
+								let mx,my = (calcmov dx (w1 +. w2)),(calcmov dy (h1 +. h2)) in
+								(* note, you only need one of these moves - rectanguar objects*)
+								let move = if (fabs mx) < (fabs my) then mx, (0. *.my) else (0. *. mx), my in
+								(* printf "move %f,%f\n%!" (fst move) (snd move) ; *)
+								m1#move (Pts2.scl move (-0.75)); (* tweak this ? *)
+								m2#move (Pts2.scl move 0.75); 
+								incr mvcnt;
+								(* clearly, this will have to run for a while ... *)
+							)
+						)
+					) amods; 
+				) amods; 
+				incr passes; 
+			) done; 
+		in
+		hitAll(); 
+		renderAll(); 
+		
 		incr k; 
 		Printf.printf "pass %d\n%!" !k; 
 	) done; 
